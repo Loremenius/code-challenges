2b329e882cdb313747b29cb7e377004330635418
Marcellino Fernandez <54911490+Loremenius@users.noreply.github.com>
2020-08-25 14:55:29 -0600

    Create brickWall.js

---
+++ brickWall.js
@@ -0,0 +1,22 @@
+var leastBricks = function(wall) {
+    const seen = {};
+    
+    wall.forEach( ( row ) => {
+        
+        let total = 0;
+        
+        for( let i = 0; i < row.length-1; i++ ){
+            
+            total += row[i];
+            
+            if( seen[total] ) seen[total] += 1;
+            else seen[total] = 1;
+            
+        }
+    });
+    
+    const amountAlign = Object.values(seen);
+    
+    if( amountAlign.length > 0 ) return wall.length - Math.max( ...amountAlign );
+    else return wall.length;
+};
